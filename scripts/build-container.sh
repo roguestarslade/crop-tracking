74df8d4a5a48360af9bc41b7a54269b5d6015be4
Slade Villena <slade@roguestar.games>
2025-05-25 02:39:32 -0500

    ok fuck C, lets go full Rust

--- scripts/build-container.sh
+++ scripts/build-container.sh
@@ -3,19 +3,19 @@
 set -e
 set -x  # ⬅️ Shell trace ON
 
-echo "🐳 Building Docker image: crop-tracking"
+echo "🐳 Building Docker image: tracking-solution"
 
 start=$(date +%s)
 
 docker build \
   --progress=plain \
-  --tag crop-tracking \
+  --tag tracking-solution \
   --file Dockerfile \
   .
 
 end=$(date +%s)
 duration=$(( end - start ))
 
-docker build -t crop-tracking:latest .
+docker build -t tracking-solution:latest .
 
 echo "✅ Done building 'crop-tracking' container in ${duration}s"
